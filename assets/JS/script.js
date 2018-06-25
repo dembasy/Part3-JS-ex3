fb6221c5ab3ce21d91d170a9745390f718b5087f
dembasy <dembasy1227@gmail.com>
2018-06-25 15:00:04 +0200

    Changement de couleur en appuyant sur la case

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+function moveB(){
+  document.getElementById('text').style.color = 'blue';
+}
+function moveG(){
+  document.getElementById('text').style.color = 'green';
+}
+function moveR(){
+  document.getElementById('text').style.color = 'red';
+}
